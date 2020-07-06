dd14cb718c88a53627c50a178d03384de62bec65
Isaac Rocha <isaacgiordani@gmail.com>
2020-07-06 19:54:10 -0300

    Alguns acertos na view e FormViewModel

--- Inventory/Models/ViewModels/EmployeeFormViewModel.cs
+++ Inventory/Models/ViewModels/EmployeeFormViewModel.cs
@@ -6,6 +6,5 @@ namespace Inventory.Models.ViewModels
     {
         public Employee Employee { get; set; }
         public ICollection<Company> Companies { get; set; }
-        public ICollection<Equipment> Equipments { get; set; }
     }
 }
